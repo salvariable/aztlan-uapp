3946af1e046b40092c211df956d70e5473740595
salvariable <salvariable@icloud.com>
2020-06-03 22:57:18 -0500

    Add RNavigation, RNPaper and landing component

--- App.js
+++ App.js
@@ -1,36 +1,37 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/Home";
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+  sizes: {
+    sm: 1,
+    md: 2,
+    lg: 3,
+  },
+  layouts: { chico: 8, mediano: 16, grande: 32 },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { StyleSheet, Text, ImageBackground, View } from "react-native";
+import { withTheme } from "react-native-paper";
+
+const Home = ({ theme }) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={{
+          uri:
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffc02.deviantart.net%2Ffs70%2Fi%2F2012%2F338%2F9%2F8%2Fwater_pattern_by_nemaakos-d5n2v50.jpg&f=1&nofb=1",
+        }}
+        style={styles.image}
+      >
+        <View style={styles.island}>
+          <Text style={styles.title}>Aztlán</Text>
+          <Text>
+            (en náhuatl: 'aztatl', ‘"garzas", y tlan, "locativo", "lugar de las
+            garzas"’) es un sitio mítico, nombrado en varias fuentes de origen
+            novohispano y anteriores, del que provienen los aztecas. Aztlán
+            suele ser identificado con una isla...
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  island: {
+    margin: 64,
+    padding: 64,
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 36,
+  },
+});
+
+export default withTheme(Home);
